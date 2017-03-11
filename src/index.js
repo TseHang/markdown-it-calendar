8614c28bbbfd465747e5825b68e7502bd769c946
Donbader <qko589999@gmail.com>
2017-03-11 12:47:13 +0800

    first commit index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-const weeks = ['SUN', 'MON', 'TUE', 'WED', 'THUS', 'FRI', 'SAT'];
+'use strict'
+require('./calendar.css')
+
+const weeks = ['SUN', 'MON', 'TUE', 'WED', 'THUS', 'FRI', 'SAT']
 const months = [
   'JANUARY',
   'FEBRUARY',
@@ -11,170 +14,280 @@ const months = [
   'SEPTEMBER',
   'OCTOBER',
   'NOVEMBER',
-  'DECEMBER',
-];
+  'DECEMBER'
+]
+exports.calendarPlugin = function (md, options) {
+  var name = 'calendar',
+    startMarkerStr = '#[' + name + '=',
+    endMarkerStr = '#[/' + name + ']',
+    DATE_REGEX = /<!--\s*(\d+)\s*-->/,
+    EVENT_REGEX = /@\[(.*?)\](.*)/
 
+/*************************************************************
+ * Default validate and render function
+ */
 
-/*
-  use zeller algorithm to count the day's date of months
-  zeller algorithm only usable after 15/10/1582
-  return 0~6, 0 = SUN, 1 = MON, ... , 6 = SAT
-*/
-function zeller(month, year) {
-  const day = 1;
-  if (month < 3) {
-    month += 12;
-    year -= 1;
-  }
-  return  ((day + parseInt(((month + 1) * 26) / 10) + year + parseInt(year / 4) + 6 * parseInt(year / 100) + parseInt(year / 400) - 1) % 7);
-}
+  function validateParamsDefault (params) {
+    // return true if params is valid
+    params = params.trim().split(' ')
+    try {
+      var year = parseInt(params[0])
+      var month = parseInt(params[1])
 
-const openDescription = (num, month, year) => {
-  closeDescription();
-
-  $(`#${month}_${year}_des_${num}`).show(250).slideDown(500);
-  $(`#${month}_${year}_grid_${num}`).data('flag', 'open');
-}
-
-const closeDescription = (month, year) => {
-  const monthTotalDay = getMonthDays(month, year);
-  const firstDay = zeller(month, year);
-
-  for (let i = firstDay ; i < (firstDay + monthTotalDay); i ++) {
-    $(`#${month}_${year}_des_${i}`).hide();
-    $(`#${month}_${year}_grid_${i}`).data('flag', 'close');
-  }
-}
-
-// calculate total day of month
-function getMonthDays(month, year) {
-  let monthTotalDay;
-
-  if (month === 2) {
-    if ((year % 400 === 0) || (year % 4 === 0 && year % 100 !== 0)) {
-      monthTotalDay = 29; // leap year
-    } else {
-      monthTotalDay = 28; // common year
+      return month <= 12 && month >= 1
+    } catch (err) {
+      return false
     }
-  } else if (month === 1 || month === 3 || month === 5 || month === 7 || month === 8 || month === 10 || month === 12) {
-    monthTotalDay = 31;
-  } else if (month === 4 || month === 6 || month === 9 || month === 11) {
-    monthTotalDay = 30;
   }
 
-  return monthTotalDay;
-}
+  function createCalendarHTML (data) {
+    var iterDate = new Date(data.Date.year, data.Date.month, 0)
+    var daysNum = iterDate.getDate()
+    iterDate.setDate(1)
+    var firstDay = iterDate.getDay()
 
+    var calendar = document.createElement('table')
+    var tr, td
+    calendar.className = 'calendar'
 
-function datesClickEvent(month, year) {
-  const firstDay = zeller(month, year);
-  const monthTotalDay = getMonthDays(month, year);
+    // init title
+    tr = calendar.insertRow()
+    td = tr.insertCell()
+    td.colSpan = '7'
+    td.innerHTML = data.Date.year + '-' + months[parseInt(data.Date.month) - 1]
 
-  for (let i = firstDay; i < (firstDay + monthTotalDay); i++) {
-    $('#' + month + '_' + year + '_grid_' + i).click(function() {
-      console.log('you click' + month + '__' + year + '_' + i);      
+    // week name row
+    tr = calendar.insertRow()
+    for (var i = 0; i < 7; ++i) {
+      td = tr.insertCell()
+      td.innerHTML = weeks[i]
+    }
 
-      // Open selected and close all others
-      ($('#' + month + '_' + year + '_grid_' + i).data('flag') === 'close' )
-      ? openDescription(i, month, year)
-      : closeDescription(month, year);
-    });
-  }
-}
-
-function calendarTableCreate(month, year, data) {
-  const tbl = document.createElement('table');
-
-  let countDay = 1;
-  let countDescription = 1;
-  let fillDay = 1;
-
-  tbl.setAttribute('id', `calendar_${month}_${year}`);
-  tbl.setAttribute('class', 'markdown-it-calendar');
-  tbl.setAttribute('border', '2');
-   //document.body.appendChild(tbl);
-
-  for (let i = 0; i < 49; i++) {
-    const tr = tbl.insertRow();
-    const monthTotalDay = getMonthDays(month, year);
-    const firstDay = zeller(month, year); 
-
-    for (let j = 0; j < 7; j++) {
-      // if the cell no use, dont generate
-      if((monthTotalDay + firstDay) < countDescription ){
-        break;
+    // blank days
+    for (var i = 0; i < firstDay; ++i) {
+      if (i % 7 == 0) {
+        tr = calendar.insertRow()
       }
-      // for date and text which need to colspan
-      if (i !== 1 && i !== 2 && i !== 10 && i !== 18 && i !== 26 && i !== 34 && i !== 42 && j > 0) {
-        break;
+      td = tr.insertCell()
+    }
+
+    // general days
+    for (var i = firstDay; i < daysNum + firstDay; ++i) {
+      var today = i - firstDay + 1
+      if (i % 7 == 0) {
+        tr = calendar.insertRow()
+      }
+      td = tr.insertCell()
+      iterDate.setDate(today)
+      td.innerHTML = today + '<br>'
+      if (data.Content[iterDate]) {
+        data.Content[iterDate].forEach((e) => {
+          td.innerHTML += e.title + ':' + e.description + '<br>'
+        })
+      }
+    }
+
+    // blank days
+    for (var i = daysNum + firstDay; i % 7 != 0; ++i) {
+      td = tr.insertCell()
+    }
+
+    return calendar.outerHTML
+  }
+
+  function renderDefault (tokens, idx, _options, env, self) {
+    const data = tokens[idx].info
+    const table = createCalendarHTML(data)
+    return '<div class="markdown-it-calendar">' + table + '</div>'
+  }
+
+  /*************************************************************
+   * Helper functions
+   */
+  function isValidDate (time) {
+    // time structure must be {year: 2017, month: 12, date: 30, time: "13:14"}
+    try {
+      var year = (time['year']).toString()
+      var month = (time['month']).toString()
+      var date = (time['date'] || '').toString()
+      var time = (time['time'] || '').toString()
+      var str = [month, date, year, time].join(' ')
+      var result = Date.parse(str)
+      if (isNaN(result)) {
+        return false
+      }
+
+      // Ensure the month is same
+      var newDate = new Date(result)
+      if (newDate.getMonth() === parseInt(month) - 1) { return newDate }
+    } catch (err) {
+      return false
+    }
+    return false
+  }
+
+  function isValidTime (str) {
+    // input str must be "xx:xx"
+    try {
+      str = str.split(':')
+      var hour = parseInt(str[0])
+      var minute = parseInt(str[1])
+      return hour < 24 && hour > -1 && minute < 60 && minute > -1
+    } catch (err) {
+      return false
+    }
+  }
+
+  function parseStartLine (src, start, end, validateFunc) {
+    // Return earlier if not match
+    var valid = src[end - 1] === ']'
+    if (!valid) {
+      return false
+    }
+
+    valid = src.substring(start, start + startMarkerStr.length) === startMarkerStr
+    if (!valid) {
+      return false
+    }
+
+    valid = validateFunc(src.substring(start + startMarkerStr.length, end - 1))
+    if (!valid) {
+      return false
+    }
+
+    var params = src.substring(start + startMarkerStr.length, end - 1).trim().split(' ')
+    return {
+      year: params[0],
+      month: params[1]
+    }
+  }
+
+  function parseEndLine (src, start, end) {
+    return src.substring(start, end).trim() == endMarkerStr
+  }
+
+  function parseDate (src, start, end, time) {
+    // extract a valid Date
+    var lineStr = src.substring(start, end).trim()
+    try {
+    // var date = lineStr.match(/\(\s*(\d+)\s*\):/)
+      var date = lineStr.match(DATE_REGEX)
+      var localTime = Object.assign({}, time)
+      localTime['date'] = parseInt(date[1])
+      return isValidDate(localTime)
+    } catch (err) {
+      return false
+    }
+    return false
+  }
+
+  function parseEvent (src, start, end) {
+    var lineStr = src.substring(start, end).trim()
+    try {
+      var event = lineStr.match(EVENT_REGEX)
+      return {
+        title: event[1],
+        description: event[2]
+      }
+    } catch (err) {
+      return false
+    }
+    return false
+  }
+
+  function addToken (state, params) {
+    var token = state.push(params.type, params.tag || 'div', params.nesting)
+    token.markup = params.markup || ''
+    token.block = params.block || true
+    token.content = params.content || ''
+    if ('info' in params) {
+      token.info = params.info
+    }
+    if ('map' in params) {
+      token.map = params.map
+    }
+    return token
+  }
+
+  /*************************************************************/
+
+  options = options || {}
+
+  var validateParam = options.validateParam || validateParamsDefault,
+    render = options.render || renderDefault
+
+  /*************************************************************
+   * Rule function
+   */
+  function calendarRule (state, startLine, endLine, silent) {
+    var currentLine,
+      autoClosed = 0,
+      token,
+      start = state.bMarks[startLine] + state.tShift[startLine],
+      end = state.eMarks[startLine],
+      renderInfo = {
+        Date: {},
+        Content: {}
+      }
+
+    var currentDay
+
+    // check the first line is correct
+    var date = parseStartLine(state.src, start, end, validateParam)
+    if (date === false) {
+      return false
+    }
+    if (silent) {
+      return true
+    }
+
+    renderInfo['Date'] = date
+
+    // iterate the lines
+    for (currentLine = startLine + 1; currentLine < endLine; ++currentLine) {
+      start = state.bMarks[currentLine] + state.tShift[currentLine]
+      end = state.eMarks[currentLine]
+
+      // Meet event line
+      event = parseEvent(state.src, start, end)
+      if (currentDay && event) {
+        renderInfo['Content'][currentDay] = renderInfo['Content'][currentDay] || []
+        renderInfo['Content'][currentDay].push(event)
+        continue
+      } // ======================================================
+
+      // Meet day line
+      var day = parseDate(state.src, start, end, date)
+      if (day) {
+        currentDay = day
+        continue
       } else {
-        const td = tr.insertCell();
-        const tags = document.createElement('p');
-        const dates = document.createElement('p');
+        currentDay = undefined
+      } // ======================================================
 
-        // calendar_title
-        if (i === 0 && j === 0) {
-          td.setAttribute('colSpan', '7');
-          td.setAttribute('class', 'calendar_title');
-          td.innerHTML = months[month - 1] + ` ${year}`;
-        }
-        // Week
-        else if (i === 1) {
-          td.setAttribute('class', 'week');
-          td.innerHTML = weeks[j];
-        }
-        // Day
-        else if (i === 2 || i === 10 || i === 18 || i === 26 || i === 34 || i === 42) {
-          td.setAttribute('class', 'day');
-          td.setAttribute('id', month + '_' + year + '_grid_' + countDay);
-          td.appendChild(dates).setAttribute('id', `${month}_${year}_dates_${countDay}`);
-          td.appendChild(tags).setAttribute('id', `${month}_${year}_tags_${countDay}` );
-          dates.setAttribute('class', 'dates');
-          tags.setAttribute('class', 'tags');
-          
-        /* // border = 0
-          if((firstDay >= countDay) || (firstDay + monthTotalDay) < countDay){
-            td.style.border = "0";
-          }
-        */
-          if((countDay > firstDay) ){
-            if(fillDay > monthTotalDay){
-                break;
-            }
-            else{
-                dates.innerHTML = fillDay++;
-            }
-          }
-          
-          // fill up tags
-          for(let k in data){
-            if((data[k].date + firstDay) == countDay){
-              tags.innerHTML += data[k].tag + "<br>";
-            }
-          }
- 
-          countDay += 1; // Grid num + 1
-        }
-        // Description, colspan for 7
-        else if (i !== 1 && i !== 2 && i !== 10 && i !== 18 && i !== 26 && i !== 34 && i !== 42) {
-          td.setAttribute('colSpan', '7');
-          td.setAttribute('class', 'calendar_desciption');
-          td.setAttribute('id', month + '_' + year + '_des_' + (countDescription));
-          //fill in description of each day
+      // Meet End of line
+      if (state.src[start] === endMarkerStr[0] && parseEndLine(state.src, start, end)) {
+        autoClosed = 1
+        break
+      } // ======================================================
+    } // end for (iterate the lines)
 
-            for(let k in data){
-              if((data[k].date + firstDay) == countDescription){
-                td.innerHTML += data[k].des + "<br>";
-              }
-            }
-            countDescription++;
-        }
-      }
-    }
+    state.line = currentLine + autoClosed
+    // add token(calendar_open) to [tokens ...]
+    token = addToken(state, {
+      type: name,
+      nesting: 0,
+      markup: startMarkerStr,
+      info: renderInfo,
+      map: [startLine, state.line],
+      content: ''
+    })
+
+    return true
   }
-  //convert Dom to html
-  console.log(tbl.outerHTML);
-  return tbl.outerHTML;
+
+  md.block.ruler.before('fence', name, calendarRule, {
+    alt: ['paragraph', 'blockquote', 'list']
+  })
+  md.renderer.rules[name] = renderDefault
 }
-
-
