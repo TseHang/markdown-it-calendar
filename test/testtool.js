8618d2117559a51aaad7173412ce6ee1e2e2649c
BW <boweong113355@gmail.com>
2017-04-01 23:36:20 +0800

    standard JS

--- test/testtool.js
+++ test/testtool.js
@@ -1,171 +1,166 @@
-require('jsdom-global')();
+require('jsdom-global')()
 
-var className_calendar = 'markdown-it-calendar';
-var className_calendar_table = 'calendar';
-var className_day = 'calendar-cell';
-var className_event_container = 'calendar-content';
-var className_event = 'calendar-content-tag';
-var className_event_description = 'calendar-content-tag-hover';
+var classNameCalendar = 'markdown-it-calendar'
+var classNameCalendarTable = 'calendar'
+var classNameDay = 'calendar-cell'
+var classNameEventContainer = 'calendar-content'
+var classNameEvent = 'calendar-content-tag'
+var classNameEventDescription = 'calendar-content-tag-hover'
 
-function get_innerHTML(html)
-{
- return work_as_dom(html,function(v){
-    return v.querySelector('div > *').innerHTML;
-  });
+function getInnerHTML (html) {
+  return workAsDOM(html, function (v) {
+    return v.querySelector('div > *').innerHTML
+  })
 }
-function apply_selector_all(html,selector)
-{
-  if(html.constructor === Array)
-  {
-    console.log('apply_selector_all called with:');
-    console.log('html:');
-    console.log(html);
-    console.log('selector:');
-    console.log(selector);
+function applySelectorAll (html, selector) {
+  if (html.constructor === Array) {
+    console.log('applySelectorAll called with:')
+    console.log('html:')
+    console.log(html)
+    console.log('selector:')
+    console.log(selector)
   }
-  var cont = document.createElement('div');
-  cont.innerHTML = html;
-  var rtn = [];
-  //console.log('cont:');
-  //console.log(cont.outerHTML);
-  [].forEach.call(cont.querySelectorAll(selector),function(v){rtn = rtn.concat(v.outerHTML);});
-  return rtn;
+  var cont = document.createElement('div')
+  cont.innerHTML = html
+  var rtn = []
+  Array.prototype.forEach.call(cont.querySelectorAll(selector), function (v) { rtn = rtn.concat(v.outerHTML) })
+  return rtn
 }
-function work_as_dom(html,func)
-{
-  var dom = document.createElement('div');
-  dom.innerHTML = html;
-  return func(dom);
+function workAsDOM (html, func) {
+  var dom = document.createElement('div')
+  dom.innerHTML = html
+  return func(dom)
 }
 
-
-function Calendar_event(html)
-{
-  if(!this || !this.constructor || this.constructor !== Calendar_event)
-    return new Calendar_event(html);
-  var selfelem = apply_selector_all(html,'.'+className_event);
-  this.title="";
-  this.description="";
-  this.has_title = false;
-  this.has_description = false;
-  this.parseSuccessful = true;
-
-  if(selfelem.length==0)
-  {
-    this.parseSuccessful = false;
-    return;
+function CalendarEvent (html) {
+  if (!this || !this.constructor || this.constructor !== CalendarEvent) {
+    return new CalendarEvent(html)
   }
-  selfelem = selfelem[0];
-  switch(apply_selector_all(selfelem,'.'+className_event_description).length)
-  {
+  var selfelem = applySelectorAll(html, '.' + classNameEvent)
+  this.title = ''
+  this.description = ''
+  this.has_title = false
+  this.has_description = false
+  this.parseSuccessful = true
+
+  if (selfelem.length === 0) {
+    this.parseSuccessful = false
+    return
+  }
+  selfelem = selfelem[0]
+  switch (applySelectorAll(selfelem, '.' + classNameEventDescription).length) {
     case 0:
-      break;
+      break
     case 1:
-      this.has_description = true;
-      this.description = get_innerHTML(apply_selector_all(selfelem,'.'+className_event_description)[0]);
-      selfelem = work_as_dom(selfelem,function(v){v.querySelectorAll('.'+className_event_description).forEach(function(v){v.remove();});return v.querySelector('.'+className_event).innerHTML;});
-      break;
+      this.has_description = true
+      this.description = getInnerHTML(applySelectorAll(selfelem, '.' + classNameEventDescription)[0])
+      selfelem = workAsDOM(selfelem, function (v) {
+        v.querySelectorAll('.' + classNameEventDescription).forEach(function (v) {
+          v.remove()
+        })
+        return v.querySelector('.' + classNameEvent).innerHTML
+      })
+      break
     default:
-      this.parseSuccessful = false;
-      selfelem = work_as_dom(selfelem,function(v){v.querySelectorAll('.'+className_event_description).forEach(function(v){v.remove();});return v.innerHTML;});
-      break;
+      this.parseSuccessful = false
+      selfelem = workAsDOM(selfelem, function (v) {
+        v.querySelectorAll('.' + classNameEventDescription).forEach(function (v) {
+          v.remove()
+        })
+        return v.innerHTML
+      })
+      break
   }
-  if(selfelem.length)
-  {
-    this.has_title = true;
-    this.title = selfelem;
+  if (selfelem.length) {
+    this.has_title = true
+    this.title = selfelem
   }
-  this.parseSuccessful = this.parseSuccessful && (this.has_title || this.has_description);
-  return;
-}//done?
+  this.parseSuccessful = this.parseSuccessful && (this.has_title || this.has_description)
+}
 
-function Calendar_day(html)
-{
-  if(!this || !this.constructor || this.constructor !== Calendar_day)
-    return new Calendar_day(html);
-
-  this.events = [];
-  this.events_count = 0;
-  this.parseSuccessful = true;
-
-  var today = apply_selector_all(html,'.calendar-cell-date');
-  if(today.length!==1)
-  {
-    this.parseSuccessful = false;
-    return;
+function CalendarDay (html) {
+  if (!this || !this.constructor || this.constructor !== CalendarDay) {
+    return new CalendarDay(html)
   }
-  today = html;//today is html of today
-  var eventCont = apply_selector_all(today,'.'+className_event_container);
-  switch(eventCont.length)
-  {
+
+  this.events = []
+  this.events_count = 0
+  this.parseSuccessful = true
+
+  var today = applySelectorAll(html, '.calendar-cell-date')
+  if (today.length !== 1) {
+    this.parseSuccessful = false
+    return
+  }
+  today = html// today is html of today
+  var eventCont = applySelectorAll(today, '.' + classNameEventContainer)
+  switch (eventCont.length) {
     case 0:
-      return;
+      return
     case 1:
-      break;
+      break
     default:
-      this.parseSuccessful = false;
-      return;
+      this.parseSuccessful = false
+      return
   }
-  eventCont = eventCont[0];
-  var that = this;
-  apply_selector_all(eventCont,'.'+className_event).forEach(function(v){
-    var ev = Calendar_event(v);
-    if(ev.parseSuccessful)
-    {
-      that.events = that.events.concat(ev);
+  eventCont = eventCont[0]
+  var that = this
+  applySelectorAll(eventCont, '.' + classNameEvent).forEach(function (v) {
+    var ev = CalendarEvent(v)
+    if (ev.parseSuccessful) {
+      that.events = that.events.concat(ev)
+    } else {
+      that.parseSuccessful = false
     }
-    else
-      that.parseSuccessful = false;
-  });
-  that.events_count = that.events.length;
-  return;
+  })
+  that.events_count = that.events.length
 }
-Calendar_day.prototype.has_event = function(){return this.events_count>0;}
+CalendarDay.prototype.has_event = function () { return this.events_count > 0 }
 
-function Calendar_Calendar(html)
-{
-  
-  if(!this || !this.constructor || this.constructor !== Calendar_Calendar)
-    return new Calendar_Calendar(html);
-
-  this.days=[];
-  this.days_count = 0;
-  this.parseSuccessful = true;
-  var tmpCalendarTableArr = apply_selector_all(html,'.'+className_calendar_table);
-  switch(tmpCalendarTableArr.length)
-  {
-    case 0:
-      this.parseSuccessful = false;
-      return;
-    case 1:
-      break;
-    default:
-      this.parseSuccessful = false;
-      return;
+function CalendarCalendar (html) {
+  if (!this || !this.constructor || this.constructor !== CalendarCalendar) {
+    return new CalendarCalendar(html)
   }
-  //tmpCalendarTableArr has only one table
-  var calendarTable = tmpCalendarTableArr[0];
 
-  var that = this;
+  this.days = []
+  this.days_count = 0
+  this.parseSuccessful = true
+  var tmpCalendarTableArr = applySelectorAll(html, '.' + classNameCalendarTable)
+  switch (tmpCalendarTableArr.length) {
+    case 0:
+      this.parseSuccessful = false
+      return
+    case 1:
+      break
+    default:
+      this.parseSuccessful = false
+      return
+  }
+  // tmpCalendarTableArr has only one table
+  var calendarTable = tmpCalendarTableArr[0]
 
-  apply_selector_all(calendarTable,'.'+className_day).forEach(function(v){
-    var tmpday = Calendar_day(v);
-    that.days = that.days.concat(tmpday);
-    ++that.days_count;
-  });
-  return;
+  var that = this
+
+  applySelectorAll(calendarTable, '.' + classNameDay).forEach(function (v) {
+    var tmpday = CalendarDay(v)
+    that.days = that.days.concat(tmpday)
+    ++that.days_count
+  })
 }
-Calendar_Calendar.prototype.day = function(n){if(n>this.days_count || n<=0)return null;return this.days[n-1];}
-
-
-function getCalendars(render_result)
-{
-  var calsArr = apply_selector_all(render_result,'.'+className_calendar);
-  var rtn = [];
-  calsArr.forEach(function(v){
-    rtn = rtn.concat(new Calendar_Calendar(v));
-  });
-  return rtn;
+CalendarCalendar.prototype.day = function (n) {
+  if (n > this.days_count || n <= 0) {
+    return null
+  }
+  return this.days[n - 1]
 }
 
-module.exports = getCalendars;
+function getCalendars (renderResult) {
+  var calsArr = applySelectorAll(renderResult, '.' + classNameCalendar)
+  var rtn = []
+  calsArr.forEach(function (v) {
+    rtn = rtn.concat(new CalendarCalendar(v))
+  })
+  return rtn
+}
+
+module.exports = getCalendars
